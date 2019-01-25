f23fa2e14bba2e30cbebaecf68e93c14edc9a49b
Wassim Katbey <fv6124@wayne.edu>
2019-01-25 00:36:47 -0500

    Created project directory using Angular CLI. Created components menu and text-entry.

---
+++ src/app/text-entry/text-entry.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TextEntryComponent } from './text-entry.component';
+
+describe('TextEntryComponent', () => {
+  let component: TextEntryComponent;
+  let fixture: ComponentFixture<TextEntryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TextEntryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TextEntryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/text-entry/text-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-entry',
+  templateUrl: './text-entry.component.html',
+  styleUrls: ['./text-entry.component.css']
+})
+export class TextEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
